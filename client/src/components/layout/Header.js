5d7b769883cf82563d8641c4f8b9e932a9d07e1e
Dave Powell <evadllewop@gmail.com>
2021-04-12 17:00:25 -0500

    adjusted css

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -8,15 +8,12 @@ function Header() {
             <div className="row">
                 <div className="d-flex flex-row-reverse bd-highlight"><img className="wave" src={Wave} alt="" />
                 </div>
-                {/* <div id="box">
-                    <div id="tile01">
-                        <div id="mask">beer likes water</div>
-                    </div> */}
+              
             </div>
             <div className="col logoLg">beer likes water</div>
-            {/* </div> */}
+           
         </div>
-        // </div > * /}
+        
     )
 }
 
